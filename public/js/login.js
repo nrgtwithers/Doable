70129b19085a6109b35bf23796099c80b5cfc0d8
Ubadah Atta <ubadahatta@Ubadahs-MacBook.fios-router.home>
2019-01-26 12:02:29 -0500

    public JS files + api routes update

---
+++ public/js/login.js
@@ -0,0 +1,34 @@
+$(function () {
+    var loginForm = $("#form-login");
+    var emailInput = $("#email-input");
+    var passwordInput = $("#password-input");
+
+    loginForm.on("submit", function (event) {
+        event.preventDefault();
+
+        var userData = {
+            email: emailInput.val().trim(),
+            password: passwordInput.val().trim()
+        };
+
+        if (!userData.email || !userData.password) {
+            return;
+        }
+
+        loginUser(userData.email, userData.password);
+        emailInput.val("");
+        passwordInput.val("");
+    });
+
+    function loginUser(email, password) {
+        $.post("/api/login", {
+            email: email,
+            password: password
+        }).then(function (data) {
+            window.location.replace(data);
+        }).catch(function (err) {
+            console.log(err);
+        });
+    }
+
+});
--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -17,13 +17,35 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+  });
+
   app.post("/api/users", function(req, res) {
     db.User.create(req.body).then(function(dbUser) {
       res.json(dbUser);
     });
   });
 
-  app.delete("/api/authors/:id", function(req, res) {
+  app.post("/api/signup", function(req, res) {
+    console.log(req.body);
+    db.User.create({ // instead of db.user it should be db.(new table for sign up/log in)
+      email: req.body.email,
+      password: req.body.password
+    }).then(function() {
+      res.redirect(307, "/api/login");
+    }).catch(function(err) {
+      console.log(err);
+      res.json(err);
+    });
+  });
+
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+    res.json("/users");
+  });
+
+  app.delete("/api/users/:id", function(req, res) {
     db.User.destroy({
       where: {
         id: req.params.id
