03ec180f24ff546aae06312f05aa33d86d3c2932
Nina Withers <ninarosewithers@gmail.com>
2019-01-28 19:35:45 -0500

    changed testimonials and images

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -1,20 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.parallax').parallax();
-  });
+});
 
-  $(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
-  });
+});
 
 
-  $("#search-button").on("click",function(){
+$("#search-button").on("click", function () {
     $.ajax("/api/tasks", {
-      type: "GET",
-  }).then(function () {
-      console.log("search button clicked")
-  })
-  })
+        type: "GET",
+    }).then(function () {
+        console.log("search button clicked")
+    })
+})
 
-  $(document).ready(function(){
+$(document).ready(function () {
     $('.modal').modal();
-  });
+});
+
+$('.dropdown-trigger').dropdown();
+
+$("#user").hide();
+
+$(".modal-close").click(function () {
+    // alert( "Handler for .click() called." );
+    // $(".modal1").hide();
+    // $(".modal2").hide();
+    // $("#testimonials").hide();
+    // $("#contact").hide();
+    $("#user").show();
+});
