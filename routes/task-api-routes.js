95273cf84669507c076c02a6e822aa2d4a63684b
Ubadah Atta <ubadahatta12@gmail.com>
2019-01-29 12:11:15 -0500

    user page features front end JS and routing

--- routes/task-api-routes.js
+++ routes/task-api-routes.js
@@ -1,66 +1,86 @@
 var db = require("../models");
 
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/api/tasks", function(req, res) {
-    var query = {};
-    if (req.query.user_id) {
-      query.UserId = req.query.user_id;
-    }
 
+  // find tasks by location
+  app.get("/api/tasks/:location", function (req, res) {
     db.Task.findAll({
-      include: [ db.User
-      ]
-  },{
-      where: query
-    }).then(function(dbTask) {
-      res.json(dbTask);
-    });
-  });
-
-
-  app.get("/api/tasks/:id", function(req, res) {
-   
-    db.Task.findOne({
       include: [{
-          model: db.User,
+        model: db.User,
       }]
-  },{
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbTask) {
-      console.log(dbtask);
+    }, {
+        where: {
+          location: req.params.location
+        }
+      }).then(function (dbTask) {
+        res.json(dbTask);
+      });
+  });
+
+
+  // find tasks by title
+  app.get("/api/tasks/:title", function (req, res) {
+    db.Task.findAll({
+      include: [{
+        model: db.User,
+      }]
+    }, {
+        where: {
+          title: req.params.location
+        }
+      }).then(function (dbTask) {
+        res.json(dbTask);
+      });
+  });
+
+
+  // show my tasks status
+  app.get("/api/tasks/:id", function (req, res) {
+    db.Task.findAll({
+      include: [{
+        model: db.User,
+      }]
+    }, {
+        where: {
+          UserId: req.params.id
+        }
+      }).then(function (dbTask) {
+        console.log(dbTask.status)
+        res.json(dbTask);
+      });
+  });
+
+  //create a new task 
+  app.post("/api/tasks", function (req, res) {
+    db.Task.create(req.body).then(function (dbTask) {
       res.json(dbTask);
     });
   });
 
-  app.post("/api/tasks", function(req, res) {
-    db.Task.create(req.body).then(function(dbTask) {
-      res.json(dbTask);
-    });
-  });
 
-  app.delete("/api/tasks/:id", function(req, res) {
+  //delete a task
+  app.delete("/api/tasks/:id", function (req, res) {
     db.Task.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function(dbPost) {
+    }).then(function (dbPost) {
       res.json(dbPost);
     });
   });
 
-  app.put("/api/tasks", function(req, res) {
+  //update task
+  app.put("/api/tasks", function (req, res) {
     db.Task.update(
       req.body,
       {
         where: {
           id: req.body.id
         }
-      }).then(function(dbTask) {
-      res.json(dbTask);
-    });
+      }).then(function (dbTask) {
+        res.json(dbTask);
+      });
   });
 };
